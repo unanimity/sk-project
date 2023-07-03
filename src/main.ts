86c1fc5ba9f7321e6a30b5e3d7c120391559423f
unanimity0 <unanimity0@gmail.com>
2023-07-03 09:33:22 -0500

    mixpanel

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+import * as mixpanel from "mixpanel-browser";
+
+mixpanel.init('777b5dfb6376ac74fd7475be8ee64426', {debug: false, track_pageview: true, persistence: 'localStorage'});
 
 if (environment.production) {
   enableProdMode();
 }
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
